c634db2690277f7118f28260f72ee47a87da069b
rapagoo <124426439+rapagoo@users.noreply.github.com>
2026-07-15 20:07:42 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -3,6 +3,4 @@ def solution(numer1, denom1, numer2, denom2):
     numer=numer1*denom2+numer2*denom1
     denom=denom1*denom2
     gcd=math.gcd(numer, denom)
-    numer//=gcd
-    denom//=gcd
-    return [numer, denom]
+    return [numer//gcd, denom//gcd]
