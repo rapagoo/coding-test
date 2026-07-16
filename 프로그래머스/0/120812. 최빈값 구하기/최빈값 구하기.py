f5034bc23c1cee09fe963c4f44d5886d051a9a03
rapagoo <124426439+rapagoo@users.noreply.github.com>
2026-07-16 15:45:19 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.06 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -0,0 +1,18 @@
+def solution(array):
+    count=[0]*1001
+    
+    for n in array:
+        count[n] += 1
+
+    max_count = -1
+    answer = -1
+    
+    for n in range(1001):
+        if count[n] > max_count:
+            max_count = count[n]
+            answer = n
+        elif count[n]==max_count:
+            answer = -1
+            
+    return answer
+            
