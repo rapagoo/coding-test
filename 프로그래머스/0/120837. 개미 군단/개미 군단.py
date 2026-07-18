53bfd0ba3aa0111a213fb3a9bfdc561ab95639a0
rapagoo <124426439+rapagoo@users.noreply.github.com>
2026-07-18 16:38:19 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.py
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -1,5 +1,2 @@
 def solution(hp):
-    first_ant = hp // 5
-    second_ant = (hp % 5) // 3
-    third_ant = hp % 5 % 3
-    return first_ant+second_ant+third_ant
+    return hp // 5 + (hp % 5) // 3 + (hp % 5) % 3
