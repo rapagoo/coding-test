2008c715d8b9f5aef7d8878d543a4cc6cbe63cdf
rapagoo <124426439+rapagoo@users.noreply.github.com>
2026-07-26 08:44:07 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,9 +1,2 @@
 def solution(numbers, direction):
-
-    if direction == "right":
-        for i in range(len(numbers)-1, 0, -1):
-            numbers[i-1], numbers[i] = numbers[i], numbers[i-1]
-    else:
-        for i in range(len(numbers)-1):
-            numbers[i+1], numbers[i] = numbers[i], numbers[i+1]
-    return numbers
+    return [numbers[-1]] + numbers[:-1] if direction=="right" else numbers[1:] + [numbers[0]]
