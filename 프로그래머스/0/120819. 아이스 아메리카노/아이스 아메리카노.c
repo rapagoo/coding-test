abdd072e2681b661c82a0bb6f228fe37bf34eb25
Sungrok Hong <124426439+rapagoo@users.noreply.github.com>
2026-08-04 16:41:15 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 5.07 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.c
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.c
@@ -4,12 +4,7 @@
 
 int answer[2]={0};
 int* solution(int money) {
-    int count = -1;
-    int result = -1;
-    while (money>=5500) {
-        money -= 5500;
-        answer[0]++;
-    }
-    answer[1] = money;
+    answer[0] = money / 5500;
+    answer[1] = money % 5500;
     return answer;
 }
